bb73a0183403b0869eadf5cc3fa5acccd4a8c187
inikanipp <pengumpulanlomba@gmail.com>
2025-03-16 16:55:42 +0700

    update PageTwo

--- nyetop/lib/main.dart
+++ nyetop/lib/main.dart
@@ -1,93 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'second_page.dart';
+import 'first_page.dart';
+
 // import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-
-        // start background
-        body: Container(
-          color: Color(0xFF060A56),
-          child: Column(
-          children: [
-            Expanded(child: 
-            Container(
-              width: 500,
-              child: Image.asset( 'assets/images/onBoard1.png',
-                    width: 80,
-                    height: 80,
-                    // fit: BoxFit.cover, // Sesuaikan ukuran gambar
-                      ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(64),
-                  bottomRight: Radius.circular(64)
-                ),
-              ),
-              // child: Container(),
-            )), 
-
-            Expanded(child: Container(
-              // color: Colors.green,
-              child: Column(
-                children: [
-                  SizedBox(height: 73),
-                  Expanded(child: 
-                    Container(
-                      height: 300,
-                      width: 350,
-                      // color:  Colors.cyanAccent,
-                      child: Column(
-                        children: [
-                          Text("Welcome to  NyeTop", style: GoogleFonts.poppins(
-                              textStyle: TextStyle(color: Colors.white, letterSpacing: .5, fontSize: 32, fontWeight: FontWeight.w600)
-                            )
-                              ),
-                          SizedBox(height: 16,),
-                          Text("Sewa laptop dari sesama mahasiswa dengan harga terjangkau dan proses cepat.",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                            textStyle: TextStyle(color: Colors.white, letterSpacing: .5, fontSize: 16, fontWeight: FontWeight.w400)),
-                          ),
-                          SizedBox(height: 128),
-                          Container(
-                            // color: Colors.red,
-                            width: 350,
-                            height: 58,
-                            child: ElevatedButton( onPressed: () {
-                                  Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => SecondPage()),
-                                  );
-                                }, child: Text('Continue', style: GoogleFonts.poppins(
-                                    textStyle: TextStyle( color: Color(0xFF060A56), fontSize: 20, fontWeight: FontWeight.w500
-                                    ) ))),
-                          )
-                        ],
-                      ),
-                    )
-                  ),
-                ],
-              )
-            )),
-          ],
-        ),
-        )
-        // end background
-      )
-    );
-  }
+      home: FirstPage() ,
+    )
+  );
 }
+
--- nyetop/lib/second_page.dart
+++ nyetop/lib/second_page.dart
@@ -1,15 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Halaman Kedua'),
+        // title: Text('Halaman Kedua qq'),
       ),
-      body: Center(
-        child: Text('Ini adalah halaman kedua'),
-      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 16,
+          ),
+          Container(
+            height: 50,
+            width: double.infinity,
+            // color: Colors.red,
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Text("Daftar Sekarang", style: GoogleFonts.poppins(
+              textStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 24)
+            )),
+          ),
+
+          Container(
+            height: 80,
+            width: double.infinity,
+            // color: Colors.green,
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Text("Isi data dirimu dan temukan laptop yang kamu butuhkan dalam hitungan menit.", style: GoogleFonts.poppins(
+              textStyle: TextStyle(color: Color(0xFFA9A4A4), fontWeight: FontWeight.normal, fontSize: 16)
+            )),
+          ),
+
+          Container(
+            height: 65,
+            width: double.infinity,
+            // color: Colors.yellow,
+            margin: EdgeInsets.only(left: 20, right: 20),
+            decoration: BoxDecoration(
+                // color: Colors.yellow,
+                color: Color(0xFFF0F0F0),
+                border: Border.all(color: Color(0xFFF0F0F0)),
+                borderRadius: BorderRadius.all(Radius.circular(26))
+              ),
+          ),
+
+          Container(
+            height: 65,
+            width: double.infinity,
+            // color: Colors.yellow,
+            margin: EdgeInsets.only(left: 20, right: 20, top: 18),
+            decoration: BoxDecoration(
+                // color: Colors.yellow,
+                color: Color(0xFFF0F0F0),
+                border: Border.all(color: Color(0xFFF0F0F0)),
+                borderRadius: BorderRadius.all(Radius.circular(26))
+              ),
+          ),
+
+          Container(
+            height: 65,
+            width: double.infinity,
+            // color: Colors.yellow,
+            margin: EdgeInsets.only(left: 20, right: 20, top: 18),
+            decoration: BoxDecoration(
+                // color: Colors.yellow,
+                color: Color(0xFFF0F0F0),
+                border: Border.all(color: Color(0xFFF0F0F0)),
+                borderRadius: BorderRadius.all(Radius.circular(26))
+              ),
+          ),
+
+          Container(
+            alignment: Alignment.center,
+            height: 65,
+            width: double.infinity,
+            // color: Colors.yellow,
+            margin: EdgeInsets.only(top : 18),
+            child: Text("Or Continue With", style: GoogleFonts.poppins(textStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w700
+            )),),
+          ),
+
+          Container(
+            height: 65,
+            width: double.infinity,
+            // color: Colors.yellow,
+            margin: EdgeInsets.only(left: 20, right: 20, top: 18),
+            decoration: BoxDecoration(
+                color: Color(0xFFF0F0F0),
+                border: Border.all(color: Color(0xFFF0F0F0)),
+                borderRadius: BorderRadius.all(Radius.circular(26))
+              ),
+          ),
+
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(top: 170),
+            height: 30,
+            width: double.infinity,
+            // color: Colors.green,
+            child: Text("Sudah punya akun? Masuk sekarang",
+            style: GoogleFonts.poppins(
+              textStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w400)
+            ),),
+          ),
+        ],
+      )
     );
   }
 }
